1ddae54ba8e8e23891b3049297113bf1199858b1
LiamPerson <34695601+LiamPerson@users.noreply.github.com>
2025-06-29 16:38:48 +0900

    Added error if YTDLP_DIRECTORY is not set

--- search-ytdlp/constants.ts
+++ search-ytdlp/constants.ts
@@ -1,3 +1,9 @@
+// Just error if the person doesn't have their .env setup
+const searchDirectory = process.env.YTDLP_DIRECTORY
+if (!searchDirectory) {
+	throw Error('You are missing your `YTDLP_DIRECTORY` in your `.env` file. Make sure you have this set up.')
+}
+
 // Enums
 export const SORT_OPTIONS = {
 	fuzzy: 'fuzzy',
@@ -7,7 +13,7 @@ export const SORT_OPTIONS = {
 }
 
 // Default configuration constants
-export const DEFAULT_SEARCH_DIRECTORY = process.env.YTDLP_DIRECTORY || ''
+export const DEFAULT_SEARCH_DIRECTORY = searchDirectory
 export const DEFAULT_WORKER_CORES_LIMIT = 8
 export const DEFAULT_RESULTS_LIMIT = 5
 export const DEFAULT_SORT = SORT_OPTIONS.normal
